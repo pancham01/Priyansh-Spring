7dad217a557783958a4e75a5f9eae34f680f6c6c
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-08-22 15:28:48 +0530

    update

--- src/main/java/seekho/spring/main/Main.java
+++ src/main/java/seekho/spring/main/Main.java
@@ -14,11 +14,6 @@ public class Main {
 		Employee bean1 = ioc.getBean("emp1",Employee.class);
 		System.out.println(bean1);
 		
-		
-		Employee bean2 = ioc.getBean("emp2",Employee.class);
-		System.out.println(bean2);
-		
-		
 	}
 
 }
